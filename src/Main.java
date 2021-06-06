7095dd27534b878a9655180566425ff6bcd46145
nf_chino <nn.chino0872@gmail.com>
2021-06-06 17:59:59 +0900

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("最初の数値を入力してください");
+        int num1 = scanner.nextInt();
+        System.out.println("次の数値を入力してください");
+        int num2 = scanner.nextInt();
+
+
     }
 }
