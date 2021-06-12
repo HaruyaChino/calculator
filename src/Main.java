2bbf0842fabfda37301ec2f5138c8834dd666434
nf_chino <nn.chino0872@gmail.com>
2021-06-12 17:55:33 +0900

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner =  new Scanner(System.in);
-        System.out.println("最初の数値を入力してください");
-        int num1 = scanner.nextInt();
-
-        System.out.println("次の数値を入力してください");
-        int num2 = scanner.nextInt();
+            Scanner sc = new Scanner(System.in);
+            System.out.println("最初の数値を入力してください");
+            String A = sc.nextLine();
+            String[] arr = A.split(" ");
 
 
     }
