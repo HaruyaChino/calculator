9aff1b15f14908352ca99ca10c60285fb0cce244
nf_chino <nn.chino0872@gmail.com>
2021-06-12 18:02:14 +0900

    Init

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("最初の数値を入力してください");
+            System.out.println("式(符号は +、-、*、/ で入力してください)：");
             String A = sc.nextLine();
             String[] arr = A.split(" ");
 
