2e5e5855cab9bbb32b5849a1426efd9a5e491013
nf_chino <nn.chino0872@gmail.com>
2021-06-19 17:56:43 +0900

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
+
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("式(符号は +、-、*、/ で入力してください)：");
-            String A = sc.nextLine();
-            String[] arr = A.split(" ");
+
+
+            System.out.println("計算式を入力してください(演算子は +、-、＊、/ の中から入力してください)");
+            System.out.print("式："); //入力式受け取り
+            String calc = sc.nextLine();
+            String[] values = calc.split(" ",0);
+            int[] numbers = new int[values.length];
+                for(int i = 0; i < values.length; i++) {
+                    try{
+                        numbers[i] =  Integer.parseInt(values[i]);
+                    }catch(NumberFormatException e) {
+                         System.out.println("正しい入力を行ってください");
+
+                    }
+
+                }
 
 
     }
