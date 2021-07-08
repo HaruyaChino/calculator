c1a8bc3d15b9e9daa240aee83671bf74b167a497
千野遥也 <nn.chino0872@gmail.com>
2021-07-08 12:45:27 +0900

    git remove int[] valuesInt git commit -m "配列valuesIntを削除し、配列valuesStringにIntegerを導入"]
    git add while
    git commit -m "計算結果表示してからのループ化を導入"

--- src/Main.java
+++ src/Main.java
@@ -7,57 +7,61 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        System.out.println("計算式を入力してください(演算子は +、-、＊、/ の中から入力してください)");
-        System.out.print("式："); //入力式受け取り
-        String inputString = null;
-        try {
-            inputString = br.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //文字列中のスペース無視
-        Objects.requireNonNull(inputString).replaceAll("　"," ").replaceAll(" ","");
-        //"end"と入力するとプロセスを閉じる
-        if(inputString.equals("end") || inputString.equals("END")) {
-            try {
-                br.close();
-                isr.close();
-            }catch(IOException e) {
-                e.printStackTrace();
-            }
-            System.out.println("計算を終了します");
-            return;
-        }
 
-            String[] valuesString = inputString.split("[^0-9]",0);
-                int[] valuesInt = Stream.of(valuesString).mapToInt(Integer::parseInt).toArray();
-                  for(int i = 0; i > 0; i++){
-                      try{
-                          valuesInt[i] = Integer.parseInt(args[i]);
-                      }catch(NumberFormatException e){
-                          System.out.println("正しく数値が入力されていません");
-                      }
-                  }
-            String[] operators = inputString.split("[0-9]",0);
-                for(int i = 0; i > 0; i++ ){
-                    if(valuesString[i].equals("+")){
+          InputStreamReader isr = new InputStreamReader(System.in);
+          BufferedReader br = new BufferedReader(isr);
+          System.out.println("計算式を入力してください(演算子は +、-、＊、/ の中から入力してください)");
+          System.out.print("式："); //入力式受け取り
+        while(true) {
+          String inputString = null;
+          try {
+              inputString = br.readLine();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+          //文字列中のスペース無視
+          Objects.requireNonNull(inputString).replaceAll("　", " ").replaceAll(" ", "");
+          //"end"と入力するとプロセスを閉じる
+          if (inputString.equals("end") || inputString.equals("END")) {
+              try {
+                  br.close();
+                  isr.close();
+              } catch (IOException e) {
+                  e.printStackTrace();
+              }
+              System.out.println("計算を終了します");
+              return;
+          }
 
-                    }else if(valuesString[i].equals("-")) {
+          String[] valuesString = inputString.split("[^0-9]", 0);
 
-                    }else if(valuesString[i].equals("/")){
+          int total = 0;
+          if(valuesString.length < 2 || ((valuesString.length != 3) && (valuesString.length % 3 != 2))){
+             System.out.println("入力式が正しくありません");
+             return;
+          }else{
+              String[] operators = inputString.split("[0-9]",0);
+                total = Integer.parseInt(valuesString[0]);
+                    for(int i = 0; i < valuesString.length;i++){
 
-                    }else if(valuesString[i].equals("*")){
-
-                    }else{
-                        System.out.println("演算子が正しく入力されていません");
-                    }
+                            if(operators.equals("*")){
+                                total = total * Integer.parseInt(valuesString[i+1]);
+                            }else if(operators.equals("/")){
+                                total = total / Integer.parseInt(valuesString[i+1]);
+                            }else if (operators.equals("+")){
+                                total = total + Integer.parseInt(valuesString[i+1]);
+                            }else if (operators.equals("-")){
+                                total = total - Integer.parseInt(valuesString[i+1]);
+                            }else{
+                                System.out.println("error");
+                                return;
                 }
+            }
+                System.out.println(total);
 
 
 
-
-
+          }
+      }
     }
 }
